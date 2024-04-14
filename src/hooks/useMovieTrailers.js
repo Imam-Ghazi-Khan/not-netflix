98ad4be65b336675e2b9ffbbe02024146b396e99
Imam-Ghazi-Khan <igkhan1710@gmail.com>
2024-04-14 21:50:48 +0530

    added movie trailer mock

--- src/hooks/useMovieTrailers.js
+++ src/hooks/useMovieTrailers.js
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { API_OPTIONS } from "../utils/constants";
 import { addTrailerVideo } from "../utils/moviesSlice";
+import movie_trailers from "../mocks/movie_trailers.json";
 
 const useMovieTrailer = (movieId) => {
   const dispatch = useDispatch();
@@ -21,8 +22,17 @@ const useMovieTrailer = (movieId) => {
     const trailer = filterData.length ? filterData[0] : json.results[0];
     dispatch(addTrailerVideo(trailer));
   };
+
+  const getMockVideos = () => {
+    const data = movie_trailers;
+    const filterData = data.results.filter((video) => video.type === "Trailer");
+    const trailer = filterData.length ? filterData[0] : data.results[0];
+    dispatch(addTrailerVideo(trailer));
+  }
+
   useEffect(() => {
     !trailerVideo && getMovieVideos();
+    getMockVideos();
   }, []);
 };
 
